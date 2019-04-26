f2bd0cb622e5a6157ad4dc643491cf20c2f1229d
钱俊 <>
2019-04-26 16:21:05 +0800

    fix 将css拆分工具换为mini-css-extract-plugin

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -2,12 +2,12 @@
 const path = require('path');
 
 // html打包插件
-const HtmlWebpackPlugin = require('_html-webpack-plugin@3.2.0@html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // css拆分插件
-const ExtractTextPlugin = require('_extract-text-webpack-plugin@4.0.0-beta.0@extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const CleanWebpackPlugin = require('_clean-webpack-plugin@1.0.1@clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     // 入口(entry)
@@ -22,20 +22,10 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             }, {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }, {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {modules: true}
-                        },
-                        {loader: 'less-loader'}
-                    ]
-                })
+                test: /\.(le|c)ss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                }, 'css-loader', 'less-loader']
             }, {
                 test: /\.(png|jpg|jepg|gif)$/,
                 use: [
@@ -54,15 +44,21 @@ module.exports = {
     },
     // 插件
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(['dist'], {
+            root: path.resolve(__dirname, '../'),//根目录
+        }),
 
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
             hash: true //用于给打包后的bundle.js加hash后缀
         }),
-        new ExtractTextPlugin('static/style/style.css')
-    ],
-    // 开发服务器配置
-    devServer: {},
-};
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: './static/css/[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+        }),
+    ]
+}
+
